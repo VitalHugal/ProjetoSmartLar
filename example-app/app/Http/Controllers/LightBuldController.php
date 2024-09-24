b022b5b6af641b42c5999041eca90118b1f68a24
Victor <victorhjsantana@gmail.com>
2024-09-24 14:11:29 -0300

    create -mrc

---
+++ example-app/app/Http/Controllers/LightBuldController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LightBuld;
+use Illuminate\Http\Request;
+
+class LightBuldController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LightBuld $lightBuld)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LightBuld $lightBuld)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LightBuld $lightBuld)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LightBuld $lightBuld)
+    {
+        //
+    }
+}
