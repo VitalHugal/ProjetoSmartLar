12d910633c401ae68b01aa15a6559ea11bc4a3f3
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-09-24 14:37:24 -0300

    create -mcr project

--- example-app/app/Models/LightBuld.php
+++ example-app/app/Models/LightBuld.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LightBuld extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'status'];
+    protected $table = 'light_bulds';
+    protected $dates = ['deleted_at'];
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|',
+            'status' => 'required|integer|in:0,1'
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'name.required' => 'Campo nome obrigatório.',
+            'status.required' => 'Campo status obrigatório.',
+            'status.in' => 'Válido apenas 0 ou 1 nesse campo.',
+        ];
+    }
 }
--- example-app/database/migrations/2024_09_24_171042_create_light_bulds_table.php
+++ example-app/database/migrations/2024_09_24_171042_create_light_bulds_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 return new class extends Migration
 {
     /**
@@ -13,7 +15,10 @@ return new class extends Migration
     {
         Schema::create('light_bulds', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 255);
+            $table->integer('status')->default(value: 0);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
